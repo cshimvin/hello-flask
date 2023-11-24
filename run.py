f61bf7a8ec68c44114f8d885d2307860c75b1379
cshimvin <cshimminvincent@gmail.com>
2023-11-24 13:23:23 +0000

    Add json file for about data

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-# Import OS from Python library
+# Import OS and json from Python library
 import os
+import json
 
 # Import Flask class and the HTML template renderer
 from flask import Flask, render_template
@@ -15,7 +16,10 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
